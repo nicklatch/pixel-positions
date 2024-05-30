2c4167ed35fb74c6517fe63eb2d53ed8646a316b
Nick Latcham <nick.latcham@proton.me>
2024-05-30 07:03:36 -0500

    wip: push to pull

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue",],
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "black": "#060606"
+            }
+        },
     }, plugins: [],
 }
 
